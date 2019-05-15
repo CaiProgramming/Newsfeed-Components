b81c8d0c508b6585f6ef40f46043944bd88ca059
littleknightgames <38872872+littleknightgames@users.noreply.github.com>
2019-05-15 12:49:04 -0400

    Finished menu comp

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,15 @@
-
+const menu = document.querySelector(".menu");
+const menuButton = document.querySelector(".menu-button");
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-}
-
-// Start Here: Create a reference to the ".menu" class
-const menu;
-// create a reference to the ".menu-button" class
-const menuButton;
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.toggle("menu--open");
+};
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", toggleMenu);
+menu.children[0].addEventListener("mouseover", event => {
+  event.target.style.cursor = "pointer";
+  event.target.style.backgroundColor = "#69ad6c";
+});
+menu.children[0].addEventListener("mouseout", event => {
+  event.target.style.backgroundColor = "#81C784";
+});
